5014109b171e3512606d69cc5f52efd0019f90d9
FouadALLAOUI <fouadallaoui70@gmail.com>
2024-09-04 15:25:16 +0100

    Get_Movies by TMDB API

---
+++ src/components/Movie.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+
+function Movie() {
+    
+    const [movieList, setMovieList] = useState([])
+
+    const getMovie = () => {
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=e6f982ddd401b7a5a7a7184e6df5f48d")
+        .then(res => res.json())
+        .then(json => setMovieList(json.results))
+    }
+
+    useEffect(()=> {
+        getMovie();
+    },[])
+
+    console.log(movieList)
+
+  return (
+    <div>
+      {movieList.map((movie)=>(
+        <img alt='' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        style={{width:"600px", height:"500px", margin:"5px"}}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default Movie
